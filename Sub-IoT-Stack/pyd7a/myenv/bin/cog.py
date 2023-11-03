79c9f6904e451be83a9774bcab6430c7b0f67b40
kvoigt <24867209@sun.ac.za>
2023-11-03 17:49:42 +0100

    Added pyd7a aswell

---
+++ Sub-IoT-Stack/pyd7a/myenv/bin/cog.py
@@ -0,0 +1,11 @@
+#!/Users/karlvoigt/Sub-IoT-Stack/pyd7a/myenv/bin/python3.11
+""" Cog content generation tool.
+    http://nedbatchelder.com/code/cog
+
+    Copyright 2004-2019, Ned Batchelder.
+"""
+
+import sys
+from cogapp import Cog
+
+sys.exit(Cog().main(sys.argv))
